be7f6f7b7f73b5dfd153efec9957084ca897b0c0
Bell Eapen <github@gulfdoctor.net>
2024-06-21 07:08:35 -0500

    fix retreiver

--- src/app/api/llmcompletion/retreiver.ts
+++ src/app/api/llmcompletion/retreiver.ts
@@ -5,25 +5,45 @@ import { RedisVectorStore } from "@langchain/redis";
 
 
 export class RedisRetreiver {
+
+    client: any;
+    embeddings: any;
+    vectorStore: any;
+
+    constructor() {
+        this.client = this.get_client();
+        this.embeddings = this.get_embedddings();
+        this.vectorStore = this.get_vectorstore();
+    }
+
   get_client = async () => {
-      const client: any = await createClient(
-      {
-        url: "redis://10.0.0.211:6379",
-      }
-    )
-      .on('error', (err: any) => console.log('Redis Client Error', err))
-      .connect();
+        const client: any = await createClient(
+        {
+            url: "redis://10.0.0.211:6379",
+        }
+        )
+        .on('error', (err: any) => console.log('Redis Client Error', err))
+        .connect();
+        return client;
+    }
 
-    const embeddings = new OllamaEmbeddings({
-        model: "all-minilm",
-        baseUrl: "http://localhost:11434", // default value
-    });
+    get_embedddings = async () => {
+        const embeddings = await new OllamaEmbeddings({
+            model: "all-minilm",
+            baseUrl: "http://localhost:11434", // default value
+        });
+        return embeddings;
+    }
 
-    const vectorStore = new RedisVectorStore(embeddings, {
-      redisClient: client,
-      indexName: "docs",
-    });
+    get_vectorstore = async () => {
+        const vectorStore = new RedisVectorStore(this.embeddings, {
+        redisClient: await this.client,
+        indexName: "docs",
+        });
+        return vectorStore;
+    }
 
-    return vectorStore.asRetriever();
-  }
+    get_retriever = () => {
+        return this.vectorStore.asRetriever();
+    }
 }
