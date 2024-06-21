a4b04347263b3eff19373ccd60a803cb9835cfba
Bell Eapen <github@gulfdoctor.net>
2024-06-21 11:25:53 -0500

    before fixing doc

--- src/app/api/llmcompletion/retreiver.ts
+++ src/app/api/llmcompletion/retreiver.ts
@@ -34,6 +34,19 @@ export class RedisRetreiver {
         });
     }
 
+    put_docs = async (docs: any) => {
+        const embeddings = this.embeddings;
+        const vectorStore = await RedisVectorStore.fromDocuments(
+        docs,
+        embeddings,
+        {
+            redisClient: await this.client,
+            indexName: "docs",
+        }
+        );
+        return vectorStore;
+    }
+
     get_vectorstore = async () => {
         const embeddings = this.embeddings;
         const vectorStore = await new RedisVectorStore(embeddings, {
--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -3,10 +3,12 @@ import { revalidatePath } from "next/cache";
 import fs from "node:fs/promises";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
+import { RedisRetreiver } from "../llmcompletion/retreiver";
+
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
-
+    const redisRetriever = new RedisRetreiver();
     const file = formData.get("file") as File;
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
@@ -15,7 +17,9 @@ export async function POST(req: Request) {
     const loader = new PDFLoader(`/tmp/${file.name}`);
 
     const docs = await loader.load();
+    await redisRetriever.put_docs(docs);
     console.log(docs)
+
     revalidatePath("/");
 
     return NextResponse.json({ status: "success" });
