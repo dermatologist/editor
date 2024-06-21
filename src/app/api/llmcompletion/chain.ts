5df9bd96049a2318c20e9a2502cd263e53fdbe03
Bell Eapen <github@gulfdoctor.net>
2024-06-21 06:58:43 -0500

    Added llm files

---
+++ src/app/api/llmcompletion/chain.ts
@@ -0,0 +1,45 @@
+import { BaseChain } from "medpromptjs";
+import { Document } from "@langchain/core/documents";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { RunnableMap, RunnablePassthrough } from "@langchain/core/runnables";
+import { RedisRetreiver } from './retreiver'
+
+export class ChainService extends BaseChain {
+
+
+
+    // ref: https://js.langchain.com/v0.1/docs/use_cases/question_answering/citations/
+    /**
+ * Format the documents into a readable string.
+ */
+    formatDocs = (input: Record<string, any>): string => {
+        const { docs } = input;
+        return (
+            "\n\n" +
+            docs
+            .map(
+                (doc: Document) =>
+                `Article title: ${doc.metadata.title}\nArticle Snippet: ${doc.pageContent}`
+            )
+            .join("\n\n")
+        );
+    };
+
+
+    async ragChain(input: any) {
+        // subchain for generating an answer once we've done retrieval
+        const answerChain = this.prompt.pipe(this.llm).pipe(new StringOutputParser());
+        const map = RunnableMap.from({
+            question: new RunnablePassthrough(),
+            docs: await new RedisRetreiver().get_client(),
+        });
+        // complete chain that calls the retriever -> formats docs to string -> runs answer subchain -> returns just the answer and retrieved docs.
+        const _chain = map
+        .assign({ context: this.formatDocs })
+        .assign({ answer: answerChain })
+        .pick(["answer", "docs"]);
+        return _chain.invoke(input);
+    }
+
+
+}
---
+++ src/app/api/llmcompletion/retreiver.ts
@@ -0,0 +1,29 @@
+import { createClient } from "redis";
+import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
+import { RedisVectorStore } from "@langchain/redis";
+
+
+
+export class RedisRetreiver {
+  get_client = async () => {
+      const client: any = await createClient(
+      {
+        url: "redis://10.0.0.211:6379",
+      }
+    )
+      .on('error', (err: any) => console.log('Redis Client Error', err))
+      .connect();
+
+    const embeddings = new OllamaEmbeddings({
+        model: "all-minilm",
+        baseUrl: "http://localhost:11434", // default value
+    });
+
+    const vectorStore = new RedisVectorStore(embeddings, {
+      redisClient: client,
+      indexName: "docs",
+    });
+
+    return vectorStore.asRetriever();
+  }
+}
