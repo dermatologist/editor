1277a35546564d0f4334da3dc9c714b2cf6b71c2
Bell Eapen <github@gulfdoctor.net>
2024-07-07 14:57:39 -0500

    feat: Add RedisVectorStore and createClient import in setindex route

--- src/app/api/setindex/route.ts
+++ src/app/api/setindex/route.ts
@@ -1,4 +1,8 @@
+import "reflect-metadata";
+
+import { RedisVectorStore } from "@langchain/redis";
 import { NextResponse } from "next/server";
+import { createClient } from "redis";
 
 import bootstrap from "../bootstrap";
 
@@ -12,6 +16,20 @@ export async function POST(req: Request) {
     const indexName = formData.get("index") as string || "";
     container.register("index-name", {
         useValue: indexName,
+    });
+    const redis_client: any = await createClient(
+        {
+            url: process.env.NEXT_PUBLIC_REDIS_URL || "redis://localhost:6379",
+        }
+        )
+        .on('error', (err: any) => console.log('Redis Client Error', err))
+    .connect();
+    const vectorstore = await new RedisVectorStore(container.resolve("embedding"), {
+        redisClient: await redis_client,
+        indexName: indexName,
+    });
+    container.register("vectorstore", {
+        useValue: vectorstore,
     });
     return NextResponse.json({ status: "success" });
   } catch (e) {
