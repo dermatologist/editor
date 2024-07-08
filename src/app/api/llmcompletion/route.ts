5968b45ed10dff2454c10d0f6fc7d0edbacb8d0a
Bell Eapen <github@gulfdoctor.net>
2024-07-08 14:29:42 -0500

    Refactor setindex route to clear container instances before registering index name

--- src/app/api/llmcompletion/route.ts
+++ src/app/api/llmcompletion/route.ts
@@ -1,17 +1,28 @@
 import { NextResponse } from "next/server";
-
+import { unstable_cache } from "next/cache";
 import bootstrap from "../bootstrap";
 import { withRateLimit } from "../utils";
 import { ChainService } from "./chain";
+
+const getChain = async (text: string) => {
+    const chain = await new ChainService(await bootstrap(), "", "", "");
+    return chain.ragChain({'question': text});
+}
+
+const getCachedChain = unstable_cache(
+    async (text: string) => getChain(text),
+    ['chain-response'],
+    {revalidate: 300}
+);
 
 export const POST = withRateLimit(async (req) => {
     const { text } = await req.json();
 
     console.log("completion input", text);
 
-    const chain = await new ChainService(await bootstrap(), "", "", "");
+    // const chain = await new ChainService(await bootstrap(), "", "", "");
 
-    const _reply = await chain.ragChain({'question': text})
+    const _reply = await getCachedChain(text);
 
     const outputText = _reply.text.replace("\n", "").replace(/\s\s+/g, ' ') + _reply.context;
 
